acf171323ade8476558ead1f2618f07a1c5abd37
angga88888 <fingerstyle88888@gmail.com>
2018-11-03 17:44:02 +0800

    update sidebar final

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -13,6 +13,7 @@
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" type="text/css" href="{{ asset('public/css/style.css')}}" >
   <link rel="stylesheet" href="{{ asset('public/css/font-awesome.min.css')}}">
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -163,7 +164,7 @@
 
   <footer class="bottom-space"></footer>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  
   <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
