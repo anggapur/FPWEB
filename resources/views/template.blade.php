d7a5968f7eda5c9588e7080e97d56a0149616bec
angga88888 <fingerstyle88888@gmail.com>
2018-11-05 13:19:15 +0800

    blanla

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -17,11 +17,11 @@
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="floatingLeft">
-      <a class="navbar-brand" href="{{url('home')}}">TUNKIN</a>
+    <div class="floatingLeftTop">
+      <!-- <a class="navbar-brand" href="{{url('home')}}">TUNKIN</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>      
+      </button>    -->   
     </div>
     <div class="floatingRight">
       <ul class="navbar-nav mr-auto">
@@ -44,6 +44,7 @@
   <section class="utama">
     <div class="navLeft">
       <div class="sidenav">
+        <a class="floatingLeft" href="{{url('home')}}"><h4>TUNKIN</h4></a>
         @if(Auth::user()->level == "admin")
         <!-- Data Master -->
           <button class="dropdown-btn">Data Master 
